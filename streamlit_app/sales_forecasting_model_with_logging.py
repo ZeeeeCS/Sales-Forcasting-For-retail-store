d58cad6f971883b56faf3080cb74568c2c49cf73
ZeeeCS <ali.ahmedali042022@gmail.com>
2025-09-04 18:10:47 +0300

    .

--- streamlit_app/sales_forecasting_model_with_logging.py
+++ streamlit_app/sales_forecasting_model_with_logging.py
@@ -162,6 +162,7 @@ def run_lstm_model(df, use_differencing=True):
             LSTM(32, activation='relu'),
             Dense(1)
         ])
+        optimizer = Adam(learning_rate=0.001) 
         model.compile(optimizer='adam', loss='mse')
         model.fit(x_train, y_train, epochs=20, verbose=0, shuffle=False)
         preds_scaled = model.predict(x_test)
